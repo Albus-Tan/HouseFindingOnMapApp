877d16f461060917651402249909d91980ca69d6
luhaoqi <1171821456@qq.com>
2022-06-27 14:13:31 +0800

    rename Selection.dart

--- lib/Selection.dart
+++ lib/Selection.dart
@@ -5,32 +5,6 @@ import 'package:bruno/bruno.dart';
 import 'package:flutter/material.dart';
 import 'config_selection.dart';
 
-void main() => runApp(const MyApp());
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  static const String _title = "range_select_example";
-
-  @override
-  Widget build(BuildContext context) {
-    // print(const JsonDecoder().convert(configSelect).runtimeType);
-    return MaterialApp(
-        home: Scaffold(
-            appBar: BrnAppBar(title: _title),
-            body: Column(
-              children: <Widget>[
-                selectionView(),
-                Container(
-                  padding: const EdgeInsets.only(top: 400),
-                  alignment: Alignment.center,
-                  child: const Text("背景内容区域"),
-                )
-              ],
-            )));
-  }
-}
-
 Widget selectionView() {
   return BrnSelectionView(
     originalSelectionData: BrnSelectionEntityListBean.fromJson(
