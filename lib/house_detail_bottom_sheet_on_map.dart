3f82cb1e7a11655e662e2aeebdd3fed4cab74d02
jiarui <wangjiarui4@outlook.com>
2022-06-28 13:55:09 +0800

    fix: code style

--- lib/house_detail_bottom_sheet_on_map.dart
+++ lib/house_detail_bottom_sheet_on_map.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'house_card.dart';
-import 'package:bruno/bruno.dart';
 
 // 地图找房页面 点击某小区后，从下方弹出的房源详情列表 sheet
 class HouseDetailBottomSheet extends StatefulWidget {
