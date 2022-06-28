6e2edbf07fd956a31d82a9871db46d0ba3622e68
jiarui <wangjiarui4@outlook.com>
2022-06-28 13:54:48 +0800

    fix: code style

--- lib/house_detail_bottom_sheet_on_map.dart
+++ lib/house_detail_bottom_sheet_on_map.dart
@@ -4,20 +4,22 @@ import 'package:bruno/bruno.dart';
 
 // 地图找房页面 点击某小区后，从下方弹出的房源详情列表 sheet
 class HouseDetailBottomSheet extends StatefulWidget {
-  const HouseDetailBottomSheet({Key? key}) : super(key: key);
+  const HouseDetailBottomSheet({
+    Key? key,
+  }) : super(key: key);
 
   @override
-  State<HouseDetailBottomSheet> createState() => _HouseDetailBottomSheetState();
+  createState() => _HouseDetailBottomSheetState();
 }
 
 class _HouseDetailBottomSheetState extends State<HouseDetailBottomSheet> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: MaterialButton(
-        child: const Text('点击展开 bottom sheet'),
-        onPressed: (){_showHouseDetailListSheet(context);},
-      ),
+    return MaterialButton(
+      child: const Text('点击展开 bottom sheet'),
+      onPressed: () {
+        _showHouseDetailListSheet(context);
+      },
     );
   }
 }
@@ -28,7 +30,8 @@ void _showHouseDetailListSheet(BuildContext context) {
     isScrollControlled: true, // set this to true
     builder: (_) {
       return DraggableScrollableSheet(
-        initialChildSize: 0.5, // 设置父容器的高度 1 ~ 0, initialChildSize必须 <= maxChildSize
+        initialChildSize: 0.5,
+        // 设置父容器的高度 1 ~ 0, initialChildSize必须 <= maxChildSize
         expand: false,
         builder: (_, controller) {
           return Container(
@@ -36,18 +39,30 @@ void _showHouseDetailListSheet(BuildContext context) {
             child: Column(
               children: [
                 const ListTile(
-                  title: Text('小区名称', style: TextStyle(
-                      color: Colors.black, fontWeight: FontWeight.w900),),
+                  title: Text(
+                    '小区名称',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w900,
+                    ),
+                  ),
                   subtitle: Text('均价·房源套数等信息'),
                   trailing: Icon(Icons.keyboard_arrow_down),
                 ),
-                Divider(),
+                const Divider(),
                 Expanded(
                   child: ListView.builder(
                     controller: controller, // set this too
                     // 长度
                     itemCount: 15,
-                    itemBuilder: (_, i) => const HouseCard(title: "title", rooms: 3, squares: 33, community: "community", price: 250, url: '',),
+                    itemBuilder: (_, i) => const HouseCard(
+                      title: "title",
+                      rooms: 3,
+                      squares: 33,
+                      community: "community",
+                      price: 250,
+                      url: '',
+                    ),
                   ),
                 ),
               ],
