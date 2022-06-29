cb2a961858540887ca425af1ddb40f4212bf7e2f
jiarui <wangjiarui4@outlook.com>
2022-06-29 09:58:35 +0800

    fix: selection_demo

--- test/demo/house_list_page_demo/selection_demo.dart
+++ test/demo/house_list_page_demo/selection_demo.dart
@@ -1,30 +1,45 @@
-
 import 'package:app/widgets/selection.dart';
 import 'package:bruno/bruno.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
   static const String _title = "range_select_example";
+  late final Widget selection;
+  bool selectionInitialized = false;
 
   @override
   Widget build(BuildContext context) {
     // print(const JsonDecoder().convert(configSelect).runtimeType);
+    selectionView('assets/json/selection.json').then(
+          (value) =>
+          setState(
+                () {
+              selection = value;
+              selectionInitialized = true;
+            },
+          ),
+    );
+    final list = <Widget>[];
+    if (selectionInitialized) {
+      list.add(selection);
+    }
     return MaterialApp(
-        home: Scaffold(
-            appBar: BrnAppBar(title: _title),
-            body: Column(
-              children: <Widget>[
-                selectionView(),
-                Container(
-                  padding: const EdgeInsets.only(top: 400),
-                  alignment: Alignment.center,
-                  child: const Text("背景内容区域"),
-                )
-              ],
-            )));
+      home: Scaffold(
+        appBar: BrnAppBar(title: _title),
+        body: Column(
+          children: list,
+        ),
+      ),
+    );
   }
 }
