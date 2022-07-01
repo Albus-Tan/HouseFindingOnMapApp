bccd31e5def46ad12fd2662a144c130d7ecf0084
Albus-Tan <tzm0425@163.com>
2022-07-01 16:57:26 +0800

    feat: finish route plan service

--- lib/service/amap_api_service/amap_api_service.dart
+++ lib/service/amap_api_service/amap_api_service.dart
@@ -7,43 +7,43 @@ import 'package:app/service/amap_api_service/route_plan/public_route_plan.dart';
 import 'package:app/service/amap_api_service/route_plan/walking_route_plan.dart';
 import 'package:http/http.dart' as http;
 
-Future<DrivingRoutePlan> fetchDrivingRoutePlan() async {
+Future<DrivingRoutePlan> fetchDrivingRoutePlan(String oriLng, String oriLat, String desLng, String desLat) async {
   final response = await http.get(
     Uri.parse('https://restapi.amap.com/v5/direction/driving?'
         'key=beba67dedb3de25a4f91da96b33c62c0&'
-        'origin=116.481028,39.989643&destination=116.434446,39.90816&'
+        'origin=$oriLng,$oriLat&destination=$desLng,$desLat&'
         'show_fields=polyline'),
   );
   final responseJson = jsonDecode(response.body);
   return DrivingRoutePlan.fromJson(responseJson);
 }
 
-Future<WalkingRoutePlan> fetchWalkingRoutePlan() async {
+Future<WalkingRoutePlan> fetchWalkingRoutePlan(String oriLng, String oriLat, String desLng, String desLat) async {
   final response = await http.get(
     Uri.parse('https://restapi.amap.com/v5/direction/walking?'
-        'origin=116.434307,39.90909&destination=116.434446,39.90816&'
+        'origin=$oriLng,$oriLat&destination=$desLng,$desLat&'
         'key=beba67dedb3de25a4f91da96b33c62c0&show_fields=polyline'),
   );
   final responseJson = jsonDecode(response.body);
   return WalkingRoutePlan.fromJson(responseJson);
 }
 
-Future<PublicRoutePlan> fetchPublicRoutePlan() async {
+Future<PublicRoutePlan> fetchPublicRoutePlan(String oriLng, String oriLat, String desLng, String desLat) async {
   final response = await http.get(
     Uri.parse('https://restapi.amap.com/v5/direction/transit/'
-        'integrated?origin=116.434307,39.90909&'
-        'destination=116.434446,39.70816&'
+        'integrated?origin=$oriLng,$oriLat&'
+        'destination=$desLng,$desLat&'
         'key=beba67dedb3de25a4f91da96b33c62c0&'
         'show_fields=polyline&city1=021&city2=021'),
-  );
+  ); // citycode 021 is for shanghai
   final responseJson = jsonDecode(response.body);
   return PublicRoutePlan.fromJson(responseJson);
 }
 
-Future<BicycleRoutePlan> fetchBicycleRoutePlan() async {
+Future<BicycleRoutePlan> fetchBicycleRoutePlan(String oriLng, String oriLat, String desLng, String desLat) async {
   final response = await http.get(
     Uri.parse('https://restapi.amap.com/v5/direction/bicycling?'
-        'origin=116.434307,39.90909&destination=116.434446,39.90816&'
+        'origin=$oriLng,$oriLat&destination=$desLng,$desLat&'
         'key=beba67dedb3de25a4f91da96b33c62c0&show_fields=polyline'),
   );
   final responseJson = jsonDecode(response.body);
--- test/demo/amap_api_demo/driving_route_plan.dart
+++ test/demo/amap_api_demo/driving_route_plan.dart
@@ -1,41 +1,8 @@
-import 'dart:async';
-import 'dart:convert';
-import 'dart:io';
+import 'package:app/service/amap_api_service/amap_api_service.dart';
 
-import 'package:app/service/amap_api_service/route_plan/driving_route_plan.dart';
-import 'package:http/http.dart' as http;
-
-Future<DrivingRoutePlan> fetchDrivingRoutePlan() async {
-
-  final response = await http.get(
-      Uri.parse('https://restapi.amap.com/v5/direction/driving?'
-          'key=beba67dedb3de25a4f91da96b33c62c0&'
-          'origin=116.481028,39.989643&destination=116.434446,39.90816&'
-          'show_fields=polyline'),
-  );
-
-  // final response = await http.post(
-  //
-  //   Uri.parse(
-  //       'https://restapi.amap.com/v5/direction/driving'),
-  //   headers: <String, String>{
-  //     'Content-Type':'application/json',
-  //   },
-  //   body: jsonEncode(<String,String>{
-  //     'key': 'beba67dedb3de25a4f91da96b33c62c0',
-  //     'origin': '116.481028,39.989643',
-  //     'destination': '116.434446,39.90816',
-  //     'show_fields': 'polyline',
-  //   }),
-  // );
-
-  final responseJson = jsonDecode(response.body);
-
-  return DrivingRoutePlan.fromJson(responseJson);
-}
 
 void main() {
-  fetchDrivingRoutePlan().then((value) => print(value.count));
+  fetchDrivingRoutePlan('116.434307','39.90909','116.434446','39.90816').then((value) => print(value.count));
 
   // Map<String, dynamic> userMap = jsonDecode(jsonString);
   // var user = User.fromJson(userMap);
