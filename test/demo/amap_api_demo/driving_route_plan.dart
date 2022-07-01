9291d955819f2249493b7c28d1b8ad151008d6e6
Albus-Tan <tzm0425@163.com>
2022-07-01 17:27:28 +0800

    feat: add usage of driving route plan service

--- test/demo/amap_api_demo/driving_route_plan.dart
+++ test/demo/amap_api_demo/driving_route_plan.dart
@@ -2,7 +2,17 @@ import 'package:app/service/amap_api_service/amap_api_service.dart';
 
 
 void main() {
-  fetchDrivingRoutePlan('116.434307','39.90909','116.434446','39.90816').then((value) => print(value.count));
+  String polyline = "";
+  fetchDrivingRoutePlan('116.434307','39.90909','116.434446','39.90816').then((value) => {
+    print(value.count),
+    print(value.route.taxiCost),
+    value.route.paths.forEach((path) => {
+      path.steps.forEach((step) => {
+        polyline = "$polyline${step.polyline};",
+      }),
+    }),
+    print(polyline.substring(0, polyline.length - 1)),
+  });
 
   // Map<String, dynamic> userMap = jsonDecode(jsonString);
   // var user = User.fromJson(userMap);
