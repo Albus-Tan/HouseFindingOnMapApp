fc5046d17d8aa3be1c3a9cfef89c9f50d0d08b15
Albus-Tan <tzm0425@163.com>
2022-07-05 17:14:04 +0800

    feat: add start end markers

--- lib/routes/map_navigation_page.dart
+++ lib/routes/map_navigation_page.dart
@@ -7,20 +7,61 @@ import '../widgets/navigation_card.dart';
 class MapNavigationPage extends StatefulWidget {
   MapNavigationPage({Key? key}) : super(key: key);
 
+  /// 导航的起始点终止点经纬度
+  final String _oriLng = "116.484307";
+  final String _oriLat = "39.90909";
+  final String _desLng = "116.434446";
+  final String _desLat = "39.90816";
+
+  /// 导航的起始点终止点名称
+  final String _oriText = "11111";
+  final String _desText = "22222";
+
   @override
   State<MapNavigationPage> createState() => _MapNavigationPageState();
 }
 
 class _MapNavigationPageState extends State<MapNavigationPage> {
-
   String _polyline = "";
   List<LatLng> _polylinePoints = [];
+
+  final Map<String, Marker> _initMarkerMap = <String, Marker>{};
+  bool _hasInitMarker = false;
+  static final String _startIconPath = 'assets/map/start.png';
+  static final String _endIconPath = 'assets/map/end.png';
+
+  @override
+  void initState() {
+    _initMarker();
+    super.initState();
+  }
+
+  void _initMarker() async {
+    if (_hasInitMarker) {
+      return;
+    }
+    final LatLng oriMarkerPosition =
+        LatLng(double.parse(widget._oriLat), double.parse(widget._oriLng));
+    final LatLng desMarkerPosition =
+        LatLng(double.parse(widget._desLat), double.parse(widget._desLng));
+    Marker oriMarker = Marker(
+        position: oriMarkerPosition,
+        icon: BitmapDescriptor.fromIconPath(_startIconPath));
+    Marker desMarker = Marker(
+        position: desMarkerPosition,
+        icon: BitmapDescriptor.fromIconPath(_endIconPath));
+    setState(() {
+      _hasInitMarker = true;
+      _initMarkerMap[oriMarker.id] = oriMarker;
+      _initMarkerMap[desMarker.id] = desMarker;
+    });
+  }
 
   void _handleNavigation(String newPolyline) {
     _polylinePoints.clear();
     print("_handleNavigation: $newPolyline");
     List<String> pointsStrs = newPolyline.split(";");
-    for(int i = 0; i < pointsStrs.length; ++i){
+    for (int i = 0; i < pointsStrs.length; ++i) {
       List<String> latlng = pointsStrs[i].split(",");
       double lng = double.parse(latlng[0]);
       double lat = double.parse(latlng[1]);
@@ -56,14 +97,18 @@ class _MapNavigationPageState extends State<MapNavigationPage> {
       body: Stack(
         children: [
           AMapWidget(
-            polylines: _polylinePoints.isEmpty ? {} : {
-              Polyline(
-                  width: 20,
-                  customTexture: BitmapDescriptor.fromIconPath('assets/map/texture_green.png'),
-                  joinType: JoinType.round,
-                  points: _polylinePoints,
-              )
-            },
+            markers: Set<Marker>.of(_initMarkerMap.values),
+            polylines: _polylinePoints.isEmpty
+                ? {}
+                : {
+                    Polyline(
+                      width: 20,
+                      customTexture: BitmapDescriptor.fromIconPath(
+                          'assets/map/texture_green.png'),
+                      joinType: JoinType.round,
+                      points: _polylinePoints,
+                    )
+                  },
           ),
           NavigationCard(
             polyline: _polyline,
