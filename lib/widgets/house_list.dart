7e0f42dd75e5463ed3788c1ce80a0638e6e4e373
Jiarui <wangjiarui4@outlook.com>
2022-08-27 23:10:00 +0800

    fix: infinite list scroll

--- lib/widgets/house_list.dart
+++ lib/widgets/house_list.dart
@@ -33,40 +33,17 @@ class HouseList extends StatefulWidget {
 }
 
 class _HouseListState extends State<HouseList> {
-  final List<HouseCard> _houseCards = <HouseCard>[];
-  final Set<int> hasFetched = {};
-  int page = 0, pageSize = 10;
-  late int totElements;
+  final _houseCards = <HouseCard>[];
+  final hasFetched = <int>{};
+  var page = 0;
+  final pageSize = 10;
+  var totElements = 0;
+  var cachedElements = 0;
+  var _isLastPage = false;
+  var isLoading = false;
 
-  bool _isLastPage = false, isLoading = false;
-
-  // List<HouseCard> getHouseCard() {
-  //   List<HouseCard> tmp = [];
-  //   if (isLast) return tmp;
-  //   for (int i = 0; i < 5; i++) {
-  //     tmp.add(
-  //       houseCardExample[Random().nextInt(houseCardExample.length)],
-  //     );
-  //   }
-  //   return tmp;
-  // }
-
-  Future<void> getPageOfHouseCard() async {
-    if (_isLastPage || hasFetched.contains(page)) return;
-    hasFetched.add(page);
-    isLoading = true;
-    await getHousePages();
-  }
-
-  Future<List<void>> getData() async {
-    return await Future.wait(
-      [
-        getPageOfHouseCard(),
-      ],
-    );
-  }
-
-  Future<void> getHousePages() async {
+  Future<void> getHouses(int page) async {
+    if (_isLastPage) return;
     await fetchHousePage(
             widget.district,
             widget.price1,
@@ -87,57 +64,62 @@ class _HouseListState extends State<HouseList> {
         },
       );
       // debugPrint("fetchHousePage: $page $_houseCards"),
+      setState(() {
+        _isLastPage = value.last!;
+        this.page = page;
+        totElements = value.totalElements!;
+        cachedElements += 10;
+        isLoading = false;
+      });
+    });
+  }
+
+  Future<void> init() async {
+    await fetchHousePage(
+            widget.district,
+            widget.price1,
+            widget.price2,
+            widget.rentType,
+            widget.rooms,
+            widget.metroLine,
+            widget.metroStation,
+            widget.keyword,
+            page,
+            pageSize)
+        .then((value) {
+      value.content?.forEach(
+        (e) {
+          _houseCards.add(
+            e.toHouseCard(),
+          );
+        },
+      );
       _isLastPage = value.last!;
-      page++;
+      page = 0;
       totElements = value.totalElements!;
+      cachedElements = 10;
       isLoading = false;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    _houseCards.clear();
-    hasFetched.clear();
-    page = 0;
-    _isLastPage = isLoading = false;
-    return FutureBuilder(
-        future: getData(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            return ListView.separated(
-              shrinkWrap: true,
-              //加了这个HouseList外面就不用了加Container或Expanded了
-              itemCount: totElements + 1,
-              padding: const EdgeInsets.all(16.0),
-              itemBuilder: (context, index) {
-                if (index >= _houseCards.length) {
-                  getPageOfHouseCard();
-                }
-                // while (isLoading) {
-                if (index >= _houseCards.length) {
-                  if (_isLastPage) {
-                    if (index == _houseCards.length) {
-                      return const Text("已经到底部了~~");
-                    }
-                    return const Divider();
-                  }
-                  if (isLoading) {
-                    if (index == _houseCards.length) {
-                      return Text("Loading~");
-                    }
-                    return const Divider();
-                  }
-                }
-                return _houseCards[index];
-                // }
-              },
-              separatorBuilder: (BuildContext context, int index) {
-                return const Divider();
-              },
-            );
-          } else {
-            return const Center(child: CircularProgressIndicator());
-          }
-        });
+    return ListView.separated(
+      shrinkWrap: true,
+      //加了这个HouseList外面就不用了加Container或Expanded了
+      itemCount: cachedElements + 1,
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context, index) {
+        if (index < cachedElements) {
+          return _houseCards[index];
+        } else {
+          getHouses(page + 1);
+          return Text("loading");
+        }
+      },
+      separatorBuilder: (BuildContext context, int index) {
+        return const Divider();
+      },
+    );
   }
 }
