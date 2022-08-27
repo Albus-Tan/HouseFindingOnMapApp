1d42b8f519105c7c68c00247ab4113c30cf5a436
Jiarui <wangjiarui4@outlook.com>
2022-08-27 20:18:16 +0800

    update: new back end address

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,5 +1,5 @@
 
 class Constants {
-  static String backend = "http://124.71.183.73:8080";
+  static String backend = "http://123.60.88.85:30000";
   //static String backend = "http://192.168.10.6:8082";
 }
